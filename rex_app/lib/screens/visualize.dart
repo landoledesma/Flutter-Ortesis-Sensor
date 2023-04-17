dcf28b76e590eec28e6772444efdccd5bae2a4a0
Lando Ledesma <lando_ledesma@outlook.com>
2023-04-17 16:06:17 -0600

    acceder a los archivos del equipo en busca de csv

--- rex_app/lib/screens/visualize.dart
+++ rex_app/lib/screens/visualize.dart
@@ -1,8 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:csv/csv.dart';
 import 'package:rex_app/screens/select_files.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
-import '../widgets_visualize/csv_graph.dart';
+class SensorData {
+  final double x;
+  final double y;
+
+  SensorData({required this.x, required this.y});
+}
 
 class PantallaDatosHistoricos extends StatefulWidget {
   @override
@@ -11,6 +20,7 @@ class PantallaDatosHistoricos extends StatefulWidget {
 }
 
 class _PantallaDatosHistoricosState extends State<PantallaDatosHistoricos> {
+  List<List<dynamic>>? _datos;
   String? _selectedFilePath;
 
   Future<void> _cargarArchivoCSV() async {
@@ -20,10 +30,38 @@ class _PantallaDatosHistoricosState extends State<PantallaDatosHistoricos> {
     );
 
     if (filePath != null) {
+      final input = new File(filePath).openRead();
+      final fields = await input
+          .transform(utf8.decoder)
+          .transform(new CsvToListConverter())
+          .toList();
+
       setState(() {
+        _datos = fields;
         _selectedFilePath = filePath;
       });
     }
+  }
+
+  SfCartesianChart _crearGrafico() {
+    List<SensorData> datos = [];
+
+    // Asumiendo que la columna 0 contiene el eje X y la columna 1 contiene el eje Y
+    for (var fila in _datos!) {
+      datos.add(SensorData(x: fila[0].toDouble(), y: fila[1].toDouble()));
+    }
+
+    return SfCartesianChart(
+      primaryXAxis: NumericAxis(),
+      primaryYAxis: NumericAxis(),
+      series: <ChartSeries>[
+        LineSeries<SensorData, double>(
+          dataSource: datos,
+          xValueMapper: (SensorData data, _) => data.x,
+          yValueMapper: (SensorData data, _) => data.y,
+        ),
+      ],
+    );
   }
 
   @override
@@ -43,8 +81,8 @@ class _PantallaDatosHistoricosState extends State<PantallaDatosHistoricos> {
           Expanded(
             child: Container(
               decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
-              child: _selectedFilePath != null
-                  ? CsvGraph(filePath: _selectedFilePath)
+              child: _datos != null
+                  ? _crearGrafico()
                   : Center(
                       child: Text('Gráfica de datos históricos del sensor'),
                     ),
