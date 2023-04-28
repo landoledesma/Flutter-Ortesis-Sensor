d38acddde424cbd34586438e3fe447770868f657
Lando Ledesma <lando_ledesma@outlook.com>
2023-04-27 21:11:59 -0600

    modularizando funciones para recibir el valor

--- rex_app/lib/widgets_plot/buttons_plot.dart
+++ rex_app/lib/widgets_plot/buttons_plot.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:rex_app/widgets_plot/send_data.dart';
 import '../screens/home.dart';
 import '../screens/visualize.dart';
 import '../widgets_conection/bluedvicemod.dart';
@@ -15,96 +16,6 @@ class RecordingButtons extends StatelessWidget {
     this.streamController,
   });
 
-  double processData(List<int> data) {
-    // Comprueba si data tiene al menos un elemento
-    if (data.isEmpty) {
-      throw Exception('La lista de datos está vacía');
-    }
-
-    // Convierte el primer elemento de la lista en un valor double
-    double doubleValue = data[0].toDouble();
-
-    return doubleValue;
-  }
-
-  void _startRecording() async {
-    deviceModel.isTransmitting = true;
-
-    // Descubrir servicios
-    List<BluetoothService> services =
-        await deviceModel.device.discoverServices();
-
-    // Obtener servicio por UUID
-    var service = services.firstWhere(
-        (s) => s.uuid == Guid("12345678-1234-5678-1234-56789ABCDEF0"));
-
-    // Obtener característica por UUID
-    var characteristic = service.characteristics.firstWhere(
-        (c) => c.uuid == Guid("12345678-1234-5678-1234-56789ABCDEF2"));
-
-    var characteristicChar = service.characteristics.firstWhere((c) =>
-        c.uuid ==
-        Guid(
-            "12345678-1234-5678-1234-56789ABCDEF1")); // Asegúrate de que este UUID coincida con _CHAR_UUID en el servidor MicroPython
-    // Reads all characteristics
-
-    // Ejemplo de cómo escribir en una característica específica
-    List<int> startCommand = [
-      0x01
-    ]; // Comando para iniciar la transmisión, adaptar según tu dispositivo
-    await characteristic.write(startCommand);
-
-// Habilitar notificaciones para la característica _CHAR_UUID
-    await characteristicChar.setNotifyValue(true);
-    // Ejemplo de cómo escuchar las notificaciones de una característica específica
-    //await characteristicChar.read();
-    characteristicChar.value.listen((value) {
-      if (deviceModel.isTransmitting) {
-        if (value.isNotEmpty) {
-          // Agrega esta condición para asegurarte de que el valor no esté vacío
-          // Aquí se reciben los datos del dispositivo BLE
-          // Convierte los datos en un valor numérico (ejemplo: double)
-          double valor =
-              processData(value); // Implementa la función processData
-
-          // Añade el valor al StreamController
-          streamController?.add(valor);
-        }
-      }
-    });
-  }
-
-  void _stopRecording() async {
-    deviceModel.isTransmitting = false;
-
-    // Descubrir servicios
-    List<BluetoothService> services =
-        await deviceModel.device.discoverServices();
-
-    // Obtener servicio por UUID
-    var service = services.firstWhere(
-        (s) => s.uuid == Guid("12345678-1234-5678-1234-56789ABCDEF0"));
-
-    // Obtener característica por UUID
-    var characteristic = service.characteristics.firstWhere(
-        (c) => c.uuid == Guid("12345678-1234-5678-1234-56789ABCDEF2"));
-
-    var characteristicChar = service.characteristics.firstWhere((c) =>
-        c.uuid ==
-        Guid(
-            "12345678-1234-5678-1234-56789ABCDEF1")); // Asegúrate de que este UUID coincida con _CHAR_UUID en el servidor MicroPython
-    // Reads all characteristics
-
-    // Ejemplo de cómo escribir en una característica específica
-    List<int> stopCommand = [
-      0x00
-    ]; // Comando para iniciar la transmisión, adaptar según tu dispositivo
-    await characteristic.write(stopCommand);
-
-    // Detiene la escucha de notificaciones para la característica
-    await characteristic.setNotifyValue(false);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -112,12 +23,12 @@ class RecordingButtons extends StatelessWidget {
       children: [
         FloatingActionButton(
           child: Icon(Icons.play_arrow),
-          onPressed: _startRecording,
+          onPressed: () => startRecording(deviceModel, streamController),
         ),
         FloatingActionButton(
           heroTag: 'start',
           child: Icon(Icons.pause),
-          onPressed: _stopRecording,
+          onPressed: () => stopRecording(deviceModel),
         ),
         FloatingActionButton(
           heroTag: 'save',
